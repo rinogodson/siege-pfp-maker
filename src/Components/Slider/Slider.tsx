1177ee378883e76611cdb3cad60466043c10b12b
Rino Godson <rinogodson@gmail.com>
2025-09-14 20:59:22 +0530

    border color fixed

--- src/Components/Slider/Slider.tsx
+++ src/Components/Slider/Slider.tsx
@@ -19,7 +19,7 @@ function Slider({
   style?: any;
 }) {
   return (
-    <div className=" border-4 border-[rgba(0,0,0,0.3)] border-dashed text-black text-2xl w-full h-30 p-3 pt-0 flex flex-col justify-end items-center rounded-[33.5px]">
+    <div className=" border-4 border-[#8B6D4E] border-dashed text-black text-2xl w-full h-30 p-3 pt-0 flex flex-col justify-end items-center rounded-[33.5px]">
       <div className="flex flex-row h-full w-full justify-between items-center px-2">
         <div className="flex justify-center items-center gap-2 py-1">
           <Spotlight />
